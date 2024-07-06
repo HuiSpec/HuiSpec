8e286287c68495a5fd2a51f197087826cec64eff
LiWenhui <liwenhuiyx@yeah.net>
2024-07-06 20:17:36 +0800

    nvim自用

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,11 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "rust", "python", "java" },
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
